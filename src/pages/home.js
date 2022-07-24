69f06b03420e2574aa2ecc4e43af4439f58f70a8
Hae Jin Jeong <haejinyi0183@gmail.com>
2022-07-23 18:28:21 -0700

    Heroku add image/position done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import leaf from "../images/leaf.png";
 
 const Home = () => {
   return (
     <div>
       <Nav />
+      <img className='leafBK' src={leaf} alt="monstera plant" />
       <Footer />
     </div>
   );
