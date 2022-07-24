5e45222c8b4d9b29a609cbe398c3710ea973f5b1
Hae Jin Jeong <haejinyi0183@gmail.com>
2022-07-24 14:33:37 -0700

    Heroku add emiPic/position

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,16 @@
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import leaf from "../images/leaf.png";
+import emiPic from "../images/emi.png";
 
 const Home = () => {
   return (
     <div>
       <Nav />
+     <div className="imageContainer">
       <img className='leafBK' src={leaf} alt="monstera plant" />
+      <img className="emiPic" src={emiPic} alt="Emi" />
+      </div>
       <Footer />
     </div>
   );
